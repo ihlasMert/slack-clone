1b439ef4b118b5ad25fe7cc874fab8fbe872bf48
ihlasMert <ihlas1mert@gmail.com>
2023-05-14 13:39:51 +0300

    added sidebar

---
+++ src/components/SideBarOption.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const SideBarOption = ({ title, Icon }) => {
+  return (
+    <SideBarOptionContainer>
+      {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SideBarOptionChannel>
+          <span>#</span>
+          {title}
+        </SideBarOptionChannel>
+      )}
+    </SideBarOptionContainer>
+  );
+};
+export default SideBarOption
+const SideBarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+  > h3 {
+    font-weight: 500;
+  }
+  > h3 > span {
+    padding: 15px;
+  }
+`;
+const SideBarOptionChannel = styled.h3`
+  padding: 10px;
+  font-weight: 300;
+`;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,59 +1,83 @@
-import { FiberManualRecord } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import CreateIcon from "@mui/icons-material/Create";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import AppsIcon from "@mui/icons-material/Apps";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import InboxIcon from "@mui/icons-material/Inbox";
+import InsertCommentIcon from "@mui/icons-material/InsertComment";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import EmergencyRecordingIcon from "@mui/icons-material/EmergencyRecording";
+import SideBarOption from "./SideBarOption";
+
 const Sidebar = () => {
   return (
-    <SidebarContainer>
+    <SideBarContainer>
       <SidebarHeader>
         <SidebarInfo>
-          <h2>İHLAS HQ</h2>
+          <h2>Hillary FAM</h2>
           <h3>
-            <FiberManualRecordIcon />
-            İHLAS MERT
+            <EmergencyRecordingIcon />
           </h3>
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
-    </SidebarContainer>
+      <SideBarOption Icon={InsertCommentIcon} title="Threads" />
+      <SideBarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SideBarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SideBarOption Icon={PeopleAltIcon} title="People and user groups" />
+      <SideBarOption Icon={AppsIcon} title="Apps" />
+      <SideBarOption Icon={FileCopyIcon} title="file browser" />
+      <SideBarOption Icon={ExpandLessIcon} title="Show less" />
+      <SideBarOption Icon={AccessAlarmIcon} title="Channels" />
+    </SideBarContainer>
   );
 };
 
 export default Sidebar;
 
-const SidebarContainer = styled.div`
-  background-color: #3f0f40;
+const SideBarContainer = styled.div`
+  background-color: var(--slack-color);
   color: white;
+  flex: 0.3;
   border-top: 1px solid #49274b;
   max-width: 260px;
-  margin-top: 35px;
-  flex: 0.3;
+  margin-top: 50px;
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
-
 const SidebarHeader = styled.div`
   display: flex;
-  border-top: 1px solid #49274b;
+  border-bottom: 1px solid #49274b;
+  border-bottom: 10px;
   padding: 13px;
 
   > .MuiSvgIcon-root {
     padding: 8px;
     color: #49274b;
     font-size: 18px;
-    background: white;
-    border-radius: 999;
+    background-color: white;
+    border-radius: 999px;
   }
 `;
+
 const SidebarInfo = styled.div`
-  flex: 1 >h2 {
+  flex: 1;
+  > h2 {
     font-size: 15px;
     font-weight: 900;
     margin-bottom: 5px;
   }
+
   > h3 {
     display: flex;
-    font-size: 13px;
     font-weight: 400;
+    font-size: 13px;
     align-items: center;
   }
   > h3 > .MuiSvgIcon-root {
